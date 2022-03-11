f0cc9be2e534111d2ba3e7c8ce39d3074690e9c1
ndrohith09 <ndrohith09@gmail.com>
2022-03-11 18:57:59 +0530

    docs

--- scripts/build.js
+++ scripts/build.js
@@ -9,7 +9,7 @@ const outputPath = './dist';
 
 async function transformSVGtoJSX(file, componentName, format) {
   try {
-  const content = await fs.readFile(`./optimized/${file}`, 'utf-8');
+  const content = await fs.readFile(`./main/${file}`, 'utf-8');
   const svgReactContent = await svgr(
     content,
     {
@@ -71,7 +71,7 @@ async function buildIcons(format = 'esm') {
 
   await fs.mkdir(outDir, { recursive: true });
 
-  const files = await fs.readdir('./optimized', 'utf-8');
+  const files = await fs.readdir('./main', 'utf-8');
 
   await Promise.all(
     files.flatMap(async (fileName) => {
